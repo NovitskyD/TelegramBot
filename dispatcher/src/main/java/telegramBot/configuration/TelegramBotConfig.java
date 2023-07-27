f3ec0110026c71f85b299975d0be1d6e0f93c5b9
Danil Novitsky <danylo.novytskyi@gmail.com>
2023-07-28 00:20:04 +0300

    Create dispatcher microservice and add Telegram bot integration

---
+++ dispatcher/src/main/java/telegramBot/configuration/TelegramBotConfig.java
@@ -0,0 +1,22 @@
+package telegramBot.configuration;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import telegramBot.controller.TelegramBot;
+
+@Configuration
+@Data
+@PropertySource("classpath:application.properties")
+public class TelegramBotConfig {
+    @Value("${bot.name}")
+    private String botName;
+    @Value("${bot.token}")
+    private String botToken;
+}
---
+++ dispatcher/src/main/java/telegramBot/controller/TelegramBot.java
@@ -0,0 +1,54 @@
+package telegramBot.controller;
+
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import telegramBot.configuration.TelegramBotConfig;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+    private final TelegramBotConfig config;
+
+    public TelegramBot(TelegramBotConfig config, UpdateProcessor updateProcessor) {
+        this.config = config;
+    }
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return config.getBotToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        var originalMessage = update.getMessage();
+        String messageText = originalMessage.getText();
+        if (messageText != null) {
+            System.out.println(messageText);
+        } else {
+            System.out.println("Received message text is null");
+        }
+
+        var response = new SendMessage();
+        response.setChatId(originalMessage.getChatId().toString());
+        response.setText("Hello from bot");
+        sendAnswerMessage(response);
+    }
+
+    public void sendAnswerMessage(SendMessage message){
+        if(message != null){
+            try {
+                execute(message);
+            } catch (TelegramApiException e ){
+                System.out.println(e);
+            }
+        }
+    }
+}
