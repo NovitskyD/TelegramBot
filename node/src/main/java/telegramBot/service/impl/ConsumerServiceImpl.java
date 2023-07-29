709ed9c2077012cf6f3e3ea6a2a1070983166fa5
Danil Novitsky <danylo.novytskyi@gmail.com>
2023-07-29 14:59:39 +0300

    Add ConsumerService, MainService, ProducerService and their implementations

---
+++ node/src/main/java/telegramBot/service/impl/ConsumerServiceImpl.java
@@ -0,0 +1,41 @@
+package telegramBot.service.impl;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import telegramBot.service.ConsumerService;
+import telegramBot.service.MainService;
+import telegramBot.service.ProducerService;
+
+import static model.RabbitQueue.*;
+
+@Service
+@Slf4j
+public class ConsumerServiceImpl implements ConsumerService {
+    private final MainService mainService;
+
+    public ConsumerServiceImpl(MainService mainService) {
+        this.mainService = mainService;
+    }
+
+    @Override
+    @RabbitListener(queues = TEXT_MESSAGE_UPDATE)
+    public void consumerTextMessageUpdates(Update update) {
+        log.debug("Node: Text message is received");
+        mainService.processTextMessage(update);
+    }
+
+    @Override
+    @RabbitListener(queues = DOC_MESSAGE_UPDATE)
+    public void consumerDocMessageUpdates(Update update) {
+        log.debug("Node: Text message is received");
+    }
+
+    @Override
+    @RabbitListener(queues = PHOTO_MESSAGE_UPDATE)
+    public void consumerPhotoMessageUpdates(Update update) {
+        log.debug("Node: Text message is received");
+    }
+}
