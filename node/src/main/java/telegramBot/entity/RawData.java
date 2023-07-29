f34a11d84ca0ea6eecdd3477023ca94047740bd5
Danil Novitsky <danylo.novytskyi@gmail.com>
2023-07-29 14:46:54 +0300

    Create new module for managing all application logic and create RawData class with Lombok and jsonb

---
+++ node/src/main/java/telegramBot/entity/RawData.java
@@ -0,0 +1,25 @@
+package telegramBot.entity;
+
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.Type;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+
+@Getter
+@Setter
+@EqualsAndHashCode(exclude = "id")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "raw_data")
+public class RawData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Type(JsonBinaryType.class)
+    @Column(columnDefinition = "jsonb")
+    private Update event;
+}
