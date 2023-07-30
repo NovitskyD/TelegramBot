2d8257a771ea5f8bee4da83da55f9f2741c199af
Danil Novitsky <danylo.novytskyi@gmail.com>
2023-07-30 15:34:32 +0300

    add annotations @Service and @Slf4j

--- dispatcher/src/main/java/telegramBot/service/impl/AnswerConsumerImpl.java
+++ dispatcher/src/main/java/telegramBot/service/impl/AnswerConsumerImpl.java
@@ -1,12 +1,16 @@
 package telegramBot.service.impl;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import telegramBot.controller.UpdateProcessor;
 import telegramBot.service.AnswerConsumer;
 
 import static model.RabbitQueue.ANSWER_MESSAGE;
 
+@Service
+@Slf4j
 public class AnswerConsumerImpl implements AnswerConsumer {
     private final UpdateProcessor updateProcessor;
 
@@ -17,6 +21,7 @@ public class AnswerConsumerImpl implements AnswerConsumer {
     @Override
     @RabbitListener(queues = ANSWER_MESSAGE)
     public void consume(SendMessage sendMessage) {
+        log.debug("SendMessageText: " + sendMessage.getText());
         updateProcessor.setView(sendMessage);
     }
 }
